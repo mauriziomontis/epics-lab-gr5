584fb853eeebe15ef9e4e8a19ff249d4758859fd
iremnlck <139512011+iremnlck@users.noreply.github.com>
2024-08-14 14:57:36 +0200

    Add files via upload
    The beamline.py file should be located in the /home/epics/bluesky/beamlinetools/beamlinetools/BEAMLINE_CONFIG directory and gaussmeter.py file should be located in the /home/epics/bluesky/beamlinetools/beamlinetools/devices directory.

---
+++ bluesky_re/beamline.py
@@ -0,0 +1,17 @@
+# Workarounds/patches
+from ophyd.signal import EpicsSignalBase
+EpicsSignalBase.set_defaults(connection_timeout= 5, timeout=200)
+
+from ophyd import EpicsMotor
+
+# standard magics
+from bluesky.magics import BlueskyMagics
+get_ipython().register_magics(BlueskyMagics)
+
+# simulated devices
+from ophyd.sim import det1, det2, det3, det4, motor1, motor2, motor, noisy_det   # two simulated detectors
+
+from beamlinetools.devices.gaussmeter import Gaussmeter
+Gauss = Gaussmeter('group5:', name='Gauss')
+Gauss.wait_for_connection()
+
